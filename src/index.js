6653332f9e15bcca35431bab5dedec5bd5b8852c
bigfoot110614 <826160425@qq.com>
2018-09-17 08:22:52 +0800

    代码拆分 异步执行

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
-import _ from 'lodash';
-// import printMe from './print.js';
-
-if (process.env.NODE_ENV !== 'production') {
-      console.log('Looks like we are in development mode!');
-}
-
-function component() {
+async function getComponent() {
+    // return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+    //     var element = document.createElement('div');
+    //
+    //     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    //
+    //     return element;
+    //
+    // }).catch(error => 'An error occurred while loading the component');
     var element = document.createElement('div');
-    var btn = document.createElement('button');
+    const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console111!';
-    // btn.onclick = printMe;
-
-    element.appendChild(btn);
 
     return element;
 }
 
-document.body.appendChild(component());
-    // if (module.hot) {
-    //    module.hot.accept('./print.js', function() {
-    //          console.log('Accepting the updated printMe module!');
-    //          printMe();
-    //        })
-    // }
+getComponent().then(component => {
+    document.body.appendChild(component);
+})
--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack')
 
 module.exports = {
     entry: {
         app: './src/index.js',
-        another: './src/another-module.js'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
